c67ab0a4b160e5727663a0599ec9453f34963630
mykolkiprono <mkiprono94@gmail.com>
2023-01-31 10:22:31 +0300

    new

---
+++ word embedding/main.py
@@ -0,0 +1,20 @@
+import os 
+import warnings
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+print(tf.__version__)
+
+def get_data():
+    pass
+
+class Embedding(keras.model):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
